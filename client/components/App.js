83c9ffe4699b2847225be33c691efc309ea5d577
whaangbuu <chardy.orofeo@yahoo.com.ph>
2017-08-02 09:54:43 +0800

    Finished hot reloading set-up

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Greetings from './Greetings';
 
 class App extends React.Component {
 
   render() {
     return(
-      <h1>Hello from react!</h1>
+      <Greetings />
     );
   }
 
