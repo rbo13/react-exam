543264da6c8ddb56d3284f3f1525ce5ea344c541
whaangbuu <chardy.orofeo@yahoo.com.ph>
2017-08-02 09:02:01 +0800

    Added webpack config

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import path from 'path';
+
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config.dev.js';
 
 let app = express();
+
+app.use(webpackMiddleware(webpack(webpackConfig)))
 
 app.set('port', process.env.PORT || 1340);
 
 app.get('/*', (req, res) => {
-  res.send("Hello World");
+  res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.listen(app.get('port'), () => console.log("Huuray! Server is running on localhost: " + app.get('port')));
