6c2cb4d6007f4ef466cb04df860e33fb1daf76d7
whaangbuu <chardy.orofeo@yahoo.com.ph>
2017-08-02 15:28:08 +0800

    Added redirection  on succcessful signup

--- client/components/signup/SignupForm.js
+++ client/components/signup/SignupForm.js
@@ -46,7 +46,9 @@ class SignupForm extends React.Component {
 
     if(this.isValid()) {
       this.props.userSignupRequest(this.state)
-          .then( () => {},
+          .then( () => {
+              this.context.router.push('/');
+          },
           ({ data }) => this.setState({ errors: data, isLoading: false })
         );
     }
@@ -123,6 +125,10 @@ class SignupForm extends React.Component {
 
 SignupForm.propTypes = {
   userSignupRequest: React.PropTypes.func.isRequired
+}
+
+SignupForm.contextTypes = {
+  router: React.PropTypes.object.isRequired
 }
 
 export default SignupForm;
